9e76e2df30867d77d43de2b8c4fd131d965b467c
sgull-dev <122727788+sgull-dev@users.noreply.github.com>
2023-11-06 15:10:26 +0200

    SFX Import/Improved Lock View

--- ui/ui_lock.gd
+++ ui/ui_lock.gd
@@ -3,6 +3,10 @@ extends Control
 var menu_open := false
 
 
+func _ready():
+	visible = false
+
+
 func _input(event):
 	if menu_open:
 		if event.is_action_pressed("ui_cancel") or event.is_action_pressed("ui_end"):
@@ -10,10 +14,12 @@ func _input(event):
 
 
 func open_menu():
+	GameData.change_game_state(GameData.GAME_STATE.MENU)
 	menu_open = true
 	visible = true
 
 
 func close_menu():
+	GameData.change_game_state(GameData.GAME_STATE.PLAY)
 	menu_open = false
 	visible = false
