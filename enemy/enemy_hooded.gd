724c727246ac2764d28b355e7086feb3ab94ad13
sgull-dev <122727788+sgull-dev@users.noreply.github.com>
2023-11-09 14:00:31 +0200

    EnemyChase Mode

--- enemy/enemy_hooded.gd
+++ enemy/enemy_hooded.gd
@@ -8,6 +8,7 @@ var move_mode = MOVE_MODE.WANDER
 @export var wander_speed : float = 150.0
 @export var chase_speed : float = 350.0
 @export var wander_delay_time : float = 1.0
+@export var chase_no_lose_time : float = 1.0
 
 #initial position of the enemy
 var init_position : Vector3
@@ -15,19 +16,27 @@ var init_position : Vector3
 var target_position : Vector3
 
 var time_from_path_finish : float
+var time_from_chase_start : float
+var nav_delay_frames = 2
 #link to HUD for displaying jumpscare/lose screen
 var lose_hud
+var player
 @onready var agent = $NavigationAgent3D
 
 
 func _ready():
 	lose_hud = GameData.get_current_stage().get_node("HUD/LoseScreen")
+	player = GameData.get_current_stage().get_node("Player")
 	#set init position
 	init_position = global_position
 	set_random_wander_pos()
 
 
 func _physics_process(delta):
+	if nav_delay_frames > 0:
+		nav_delay_frames -= 1
+		return
+	
 	var move_v
 	#handle movement code
 	#if agent has reached end of path, stop movement for a while, then get new target pos
@@ -38,10 +47,24 @@ func _physics_process(delta):
 		if time_from_path_finish >= wander_delay_time:
 			set_random_wander_pos()
 			time_from_path_finish = 0.0
-	else:
+	elif move_mode == MOVE_MODE.WANDER:
 		var next_path_pos = agent.get_next_path_position()
 		var dir_v = (next_path_pos - global_position).normalized()
 		move_v = get_speed() * dir_v * delta
+	elif move_mode == MOVE_MODE.CHASE:
+		#set target to player
+		agent.target_position = player.global_position
+		#set movement
+		var next_path_pos = agent.get_next_path_position()
+		var dir_v = (next_path_pos - global_position).normalized()
+		move_v = get_speed() * dir_v * delta
+		#add time to chase start
+		time_from_chase_start += delta
+		if time_from_chase_start >= chase_no_lose_time:
+			#if larger than chase no lose time, start checking if player has gotten outside see radius
+			if (global_position - player.global_position).length() > $PlayerSeeArea/CollisionShape3D.shape.radius:
+				move_mode = MOVE_MODE.WANDER
+				set_random_wander_pos()
 	
 	#apply smoothing to movement
 	velocity.x = lerp(velocity.x, move_v.x, 0.2)
@@ -81,3 +104,13 @@ func get_speed() -> float:
 		return wander_speed
 	else:
 		return chase_speed
+
+
+func spot_player():
+	move_mode = MOVE_MODE.CHASE
+	time_from_chase_start = 0.0
+
+
+func _on_player_see_area_body_entered(body):
+	if "is_player" in body:
+		spot_player()
